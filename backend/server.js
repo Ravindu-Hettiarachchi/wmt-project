ea7ece7ea9dc83798fdd91e32eecaad19cd4cd93
ravinduhettiarachchi <ravinduhettiarachchi132@gmail.com>
2026-04-29 18:15:38 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import itemRoutes from "./routes/itemRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Item Manager API is running..." });
+});
+
+app.use("/api/items", itemRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error.message);
+  });
